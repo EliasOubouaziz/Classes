a3052ce1853eec87ba18daf837d6a6a7cb3ca62c
EliasOubouaziz <49237345+EliasOubouaziz@users.noreply.github.com>
2020-02-25 13:02:43 +0100

    calculRentabilité

--- projetJava/Developpement/projetJava/src/main/java/fr/miage/toulouse/dev/ChaineDeProd.java
+++ projetJava/Developpement/projetJava/src/main/java/fr/miage/toulouse/dev/ChaineDeProd.java
@@ -1,6 +1,15 @@
 package fr.miage.toulouse.dev;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 
 public class ChaineDeProd {
 
@@ -16,6 +25,46 @@ public class ChaineDeProd {
 		this.entree = new HashMap <String, Double>();
 		this.sortie = new HashMap <String, Double>();
 		this.activation = 0;
+	}
+	
+	public double calculRentabilite(String URL) {
+		double qte;
+		double somme=0;
+		Achats ach = new Achats();
+		Path orderPath = Paths.get(URL);
+		List<String> lines = null;
+		try {
+			lines = Files.readAllLines(orderPath);
+		} catch (IOException e) {
+			System.out.println("Impossible de lire le fichier des commandes");
+		}
+		for(int i = 1; i<lines.size(); i++) {
+			String[] split = lines.get(i).split(";");
+			String idE = String.valueOf(split[0]);
+			String prixE = String.valueOf(split[1]);
+			if(prixE.lastIndexOf("NA")==-1) {
+					ach.prix.put(idE, prixE);
+			}
+		}
+		
+		
+		Set listKeys = this.entree.keySet(); // Obtenir la liste des clés
+		Iterator iterateur = listKeys.iterator();
+		// Parcourir les clés 
+		while (iterateur.hasNext()) {
+			Object key = iterateur.next();
+			qte=this.entree.get(key);
+			Set listKeys2 = ach.prix.keySet(); // Obtenir la liste des clés
+			Iterator iterateur2 = listKeys2.iterator();
+			while (iterateur2.hasNext()) {
+				Object key2 = iterateur.next();
+				if(key.equals(key2)) {
+					somme = somme + qte * Double.parseDouble(ach.prix.get(key));
+				}
+			}
+		}
+		
+		return somme;
 	}
 
 	public String getId() {
