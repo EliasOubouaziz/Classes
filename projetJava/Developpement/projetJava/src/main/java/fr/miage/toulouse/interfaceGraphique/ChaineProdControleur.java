6a19378001dcf1c5a3320119078c77274c5eecf6
R0bin-sys <60320148+R0bin-sys@users.noreply.github.com>
2020-03-23 20:13:24 +0100

    Modifs boutons

--- projetJava/Developpement/projetJava/src/main/java/fr/miage/toulouse/interfaceGraphique/ChaineProdControleur.java
+++ projetJava/Developpement/projetJava/src/main/java/fr/miage/toulouse/interfaceGraphique/ChaineProdControleur.java
@@ -273,37 +273,28 @@ public class ChaineProdControleur {
 	/**
 	 * 
 	 * @param e
+	 * @throws IOException
 	 */
-	public void clicValidation(ActionEvent e) {
+	public void clicValidation(ActionEvent e) throws IOException {
 		System.out.println("Clic sur btnValider - Popup Validation");
 
-		Alert alert = new Alert(AlertType.CONFIRMATION);
-		alert.setTitle("Valider");
-		alert.setHeaderText("Sauvegarde des achats, voulez-vous garder les chaines de production ?");
+		// Fermeture de la fenetre
+		Stage fen = (Stage) btnValider.getScene().getWindow();
 
-		ButtonType oui = new ButtonType("OUI");
-		ButtonType non = new ButtonType("NON");
-		ButtonType annuler = new ButtonType("ANNULER");
+		fen.close();
 
-		// Remove default ButtonTypes
-		alert.getButtonTypes().clear();
+		// Chargement de la scène suivante
+		FXMLLoader fxmlLoader = new FXMLLoader(
+				getClass().getResource("/fr/miage/toulouse/interfaceGraphique/CommandeScene.fxml"));
+		Parent root1 = (Parent) fxmlLoader.load();
 
-		alert.getButtonTypes().addAll(oui, non, annuler);
-
-		// option != null.
-		Optional<ButtonType> option = alert.showAndWait();
-
-		if (option.get() == null) {
-			System.out.println("NULL");
-		} else if (option.get() == oui) {
-			System.out.println("OUI");
-		} else if (option.get() == non) {
-			System.out.println("NON");
-		} else if (option.get() == annuler) {
-			System.out.println("Annuler");
-		} else {
-			System.out.println("rien");
-		}
+		// Création de la nouvelle fenêtre
+		Stage newFen = new Stage();
+		newFen.setTitle("Saisie Commande");
+		newFen.setScene(new Scene(root1));
+		newFen.setResizable(false);
+		newFen.show();
+		newFen.centerOnScreen();
 	}
 
 }
